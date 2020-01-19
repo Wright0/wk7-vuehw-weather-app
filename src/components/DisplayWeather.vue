b73a9717bc4509192f2f0313bd39a1f22843c0b3
Olivia <olivia.wright101@gmail.com>
2020-01-19 12:25:10 +0000

    Component set up and linking to app

---
+++ src/components/DisplayWeather.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+  <h1>Display says hello</h1>
+</template>
+
+<script>
+export default {
+  name: "display-weather"
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/SelectDay.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+  <h1>select day says hello</h1>
+</template>
+
+<script>
+export default {
+  name: "select-day"
+}
+</script>
+
+<style lang="css" scoped>
+</style>
