19c4496d92e25c66d2d1a3cd3925c6215f7c458d
Olivia <olivia.wright101@gmail.com>
2020-01-19 12:27:10 +0000

    Component set up and linking to app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello</h1>
+    <select-day />
+    <display-weather />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SelectDay from './components/SelectDay.vue'
+import DisplayWeather from './components/DisplayWeather.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    "select-day": SelectDay,
+    "display-weather": DisplayWeather
+  },
+
 }
 </script>
 
